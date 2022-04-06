c1e8d7d72fe24143672570c80e010ad4a279d813
cryptodapp <96749852+kenjicncr@users.noreply.github.com>
2022-04-06 16:36:57 -0400

    chore: initialize

---
+++ src/mapping.ts
@@ -0,0 +1,22 @@
+import { Address, BigInt, ByteArray, Bytes, log, Value } from '@graphprotocol/graph-ts'
+import { ItemBought } from './types/KanariaDutchAuction/dutchAuctionAbi'
+import { BoughtSlot } from './types/schema'
+
+export function handleItemBought(event: ItemBought): void  {
+  let itemId: string = event.params.itemId.toString();
+  let purchaser: Address = event.params.purchaser;
+  let price: BigInt = event.params.price;
+  let timestamp: BigInt = event.block.timestamp;
+  let txHash: string = event.transaction.hash.toString();
+
+  let boughtSlot = BoughtSlot.load(txHash)
+
+  if(boughtSlot === null) {
+    boughtSlot = new BoughtSlot(txHash)
+    boughtSlot.price = price
+    boughtSlot.itemId = itemId
+    boughtSlot.purchaser = purchaser
+    boughtSlot.timeBought = timestamp
+    boughtSlot.save()
+  }
+}
